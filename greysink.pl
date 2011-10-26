d8d0550fd4c5610c115ea8e389a4d1544c5fe51a
Richard Harman <richard@richardharman.com>
2011-10-25 23:18:18 -0400

    Cleanup, add comments & implement better resolver skipping
    - Added IGNORE RCODE so we can tell when a programmable resolver says "skip me"
    - Added example NS/A records for "us" - the authorative NS for sinkholed domains
    -

--- greysink.pl
+++ greysink.pl
@@ -9,36 +9,56 @@ use Net::DNS::Resolver;
 use Tree::Trie;
 use List::Util qw(first);
 
+# (ab)use some unassigned RCODE values for our own internal use
+# IGNORE/11 is an internal identifier for ::Resolver::Programmable resolver_code says "skip me please"
+$Net::DNS::rcodesbyname{IGNORE} = 11;
+%Net::DNS::rcodesbyval = reverse %Net::DNS::rcodesbyname;
+
 # shortcut for reversing a string
 sub rev ($) { scalar reverse $_[0]; }
 
+# debugging messages
 my $verbose = 1;
 
 my $sinkhole_tree  = Tree::Trie->new();
 my $whitelist_tree = Tree::Trie->new();
 
-# in Net::DNS::RR->new() (from string) format
-# The script will replace * with the name domain name queried.
-
+# RR record values are in Net::DNS::RR->new() (from string) format
+# The script will replace * with the domain name queried.
+# { records => { RR => '* string' } }
 my $sinkhole_records = { # {{{
   records => {
 	A  => '* 86400 IN A 10.1.2.3',
-	NS => '* IN NS ns.sinkhole.example.com',
-	SOA => '* IN SOA ns.sinkhole.example.com. cert.example.com.  ( 42 28800 14400 3600000 86400)',
+	NS => '* 86400 IN NS ns.sinkhole.example.com',
+	SOA => '* 86400 IN SOA ns.sinkhole.example.com. cert.example.com.  ( 42 28800 14400 3600000 86400)',
   },
 }; # }}}
 
-# whitelist (don't add any records, we will recurse out to the internet for them)
-$whitelist_tree->add( rev('www.richardharman.com') );
+# records for glue (authority) for NS
+# ns.sinkhole.example.com == 192.168.100.100 here.
+my $authorative_ns_records = { # {{{
+  records => {
+    A => '* 86400 IN A 192.168.100.100',
+    NS => '* 86400 IN NS ns.sinkhole.example.com',
+	SOA => '* 86400 IN SOA ns.sinkhole.example.com. cert.example.com.  ( 42 28800 14400 3600000 86400)',
+  },
+}; # }}}
 
-# sinkhole (pass a hashref of RR => 'rr string') under the records key
+# whitelist: don't add any records, only add domains - we will recurse out to the internet for the records {{{
+$whitelist_tree->add( rev('www.richardharman.com') );
+# }}}
+
+# sinkhole: pass a hashref of { records => { RR => 'rr string' } } # {{{
+# XXX Note! x.com does not match *.x.com.  Wildcards are EXPLICIT, not implied.
 $sinkhole_tree->add_data( rev('richardharman.com'),  $sinkhole_records  );
 $sinkhole_tree->add_data( rev('*.richardharman.com'), $sinkhole_records  );
-$sinkhole_tree->add_data( rev('sinkhole.example.com'),  $sinkhole_records );
-$sinkhole_tree->add_data( rev('*.sinkhole.example.com'), $sinkhole_records );
-$sinkhole_tree->add_data( rev('*.dyn.com'), $sinkhole_records  );
 $sinkhole_tree->add_data( rev('dyn.com'),  $sinkhole_records  );
+$sinkhole_tree->add_data( rev('*.dyn.com'), $sinkhole_records  );
+# }}}
 
+# authorative NS server records, for zones we're sinkholing.
+# XXX FIXME: this will be more effective/useful when ::Resolver::Programmable supports additional/authority RRs.
+$sinkhole_tree->add_data( rev('ns.sinkhole.example.com'),  $authorative_ns_records  );
 
 my $recursive = Net::DNS::Resolver->new( # {{{
   recursive => 1,
@@ -53,8 +73,10 @@ my $whitelist = Net::DNS::Resolver::Programmable->new( # {{{
   resolver_code => \&whitelist_handler,
 ); # }}}
 
-# resolvers - order matters here.
-# first one to respond is the response that gets sent to the client.
+# resolvers - order matters here.  Priority first.
+# first one to respond with an RCODE not equal to IGNORE is the response that gets sent to the client.
+#
+# XXX remove $recursive here if you don't want this nameserver to be an open dns server.
 my @resolvers = ($whitelist,$sinkhole,$recursive);
 
 # our nameserver object.  Query me w/ 'dig -p 5252 -t a mtfnpy.dyn.com @localhost'
@@ -65,88 +87,94 @@ my $ns = Net::DNS::Nameserver->new( # {{{
   Verbose      => $verbose,
 ) || die "couldn't create nameserver object ($!)"; # }}}
 
+# our handler for all incoming DNS requests.  Here's the brains.
 sub reply_handler { # {{{
   my ( $qname, $qclass, $qtype, $peerhost, $query, $conn ) = @_;
-  my ( $rcode, @ans, @auth, @add );
+  my ( $rcode, @ans, @auth, @add, $aa );
 
   # send requet to various resolvers
   my (@responses) = map { $_->send( $qname, $qtype, $qclass ) } @resolvers;
 
-  # we only want the first response
-  my $valid_answer = first { $_->header->ancount } @responses;
+  # we only want the first valid response - we use a special sauce dns RCODE to skip resolvers
+  my $response = first { $_->header->rcode ne "IGNORE" } @responses;
 
-  # response might be undef if nothing responded
-  if ($valid_answer) { # response was valid {{{
+  # $response might be undef if nothing responded
+  if ($response) { # response was valid {{{
     # XXX FIXME RGH: Net::DNS::Resolver::Programmable doesn't support sending additional or authorative records :(
-    $rcode = $valid_answer->header->rcode;
-    @ans   = $valid_answer->answer;
-    @add   = $valid_answer->additional;
-    @auth  = $valid_answer->authority;
+    $rcode = $response->header->rcode;
+    @ans   = $response->answer;
+    @add   = $response->additional;
+    @auth  = $response->authority;
   } # }}}
   else { # none of our resolvers found anything {{{
-    $rcode = "SERVFAIL";
+    # return NXDOMAIN: because either the sinkhole/whitelist don't have the record
+    # or the recursive resolver didn't find anything.
+    $rcode = "NXDOMAIN";
   } # }}}
 
   # XXX FIXME We need to sanatise responses!
   # The glue records (@add) might override what we "know" to be the "truth"
   # (e.g. what we have blacklisted, and how to "get" there)
-  return ( $rcode, \@ans, \@auth, \@add, );
+  return ( $rcode, \@ans, \@auth, \@add, $aa );
 
 } # }}}
 
 sub sinkhole_handler { # {{{
-  my ( $domain, $rr_type, $class ) = @_;
-  my ( $result, $aa, @rrs );
+  my ( $qname, $qtype, $qclass ) = @_;
+  my ( $rcode, $aa, @rrs );
 
-  my $zone = first { $sinkhole_tree->lookup( rev lc($_) ) } wildcardsearch($domain);
+  my $zone = first { $sinkhole_tree->lookup( rev lc($_) ) } wildcardsearch($qname);
   if ($zone) { # we found a record in our tree # {{{
     # grab the hashref that has our RR types & records
     my $record = $sinkhole_tree->lookup_data( rev lc($zone) );
 
     # check if the RR type we want exists
-    if ( exists( $$record{records}{$rr_type} ) ) { # RR exists, now we get to answer {{{
-      my $str = $record->{records}->{$rr_type};
+    if ( exists( $$record{records}{$qtype} ) ) { # RR exists, now we get to answer {{{
+      my $str = $record->{records}->{$qtype};
 
       # make our sinkholed response look like the question
-      $str =~ s/\*/$domain/g;
+      $str =~ s/\*/$qname/g;
       push @rrs, Net::DNS::RR->new($str);
-      $result = "NOERROR";
+      $rcode = "NOERROR";
     } # }}}
     else { # zone exists, but not the record we want. {{{
-      $result = "NXDOMAIN";
+      $rcode = "NXDOMAIN";
     } # }}}
   } # }}}
-  else { # we didn't find any records, so NXDOMAIN {{{
-    $result = "NXDOMAIN";
+  else { # we didn't find any records, so return custom rcode IGNORE {{{
+    $rcode = "IGNORE";
   } # }}}
 
   # XXX FIXME
   # it would be nice if we could respond with additional RRs (for NS lookups)
-  return ( $result, $aa, @rrs );
+  return ( $rcode, $aa, @rrs );
 } # }}}
 
 sub whitelist_handler { # {{{
-  my ( $domain, $rr_type, $class ) = @_;
-  my ( $result, $aa, @rrs );
+  my ( $qname, $qtype, $qclass ) = @_;
+  my ( $rcode, $aa, @rrs );
 
-  my $zone = first { $whitelist_tree->lookup( rev lc($_) ) } wildcardsearch($domain);
+  my $zone = first { $whitelist_tree->lookup( rev lc($_) ) } wildcardsearch($qname);
   # $zone might be undef if no responses
   if ($zone) { # response was found {{{
-    my $answer = $recursive->send( $domain, $rr_type, $class );
+    my $answer = $recursive->send( $qname, $qtype, $qclass );
 
     # set RR Code
-    $result = $answer->header->rcode;
-    @rrs    = $answer->answer;
-    $result = "NOERROR";
+    $rcode = $answer->header->rcode;
+    @rrs   = $answer->answer;
+    $rcode = "NOERROR";
   } # }}}
-  else { # no response, return SERVFAIL.  Hopefully DNS client will come back later. # {{{
-    $result = "SERVFAIL";
+  else { # no zone found in our trie, return custom rcode IGNORE
+    $rcode = "IGNORE";
   } # }}}
 
-  return ( $result, $aa, @rrs );
+  return ( $rcode, $aa, @rrs );
 } # }}}
 
-sub wildcardsearch { # wildcard-ify a request to see if something shorter for a wildcard exists {{{
+# wildcard-ify a request to see if something shorter for a wildcard exists.
+# This operates in a "most specific" to "least specific" order. {{{
+# lookup for x.y.z.com == x.y.z.com, *.y.z.com, *.z.com, *.com
+sub wildcardsearch {
   my ($domain) = @_;
   my @parts = reverse( split( m/\./, $domain ) );
   my @wildcards = reverse map { join( ".", '*', reverse( @parts[ 0 .. $_ ] ), ) } 0 .. $#parts - 1;
