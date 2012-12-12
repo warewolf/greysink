ee9da4b44889d78936764c15277bc2ee7325cae1
Richard Harman <richard@richardharman.com>
2012-12-12 16:49:47 -0500

    More development work

--- poe/lib/POE/Component/Greysink/Server.pm
+++ poe/lib/POE/Component/Greysink/Server.pm
@@ -1,5 +1,5 @@
 package POE::Component::Greysink::Server;
-use POE;
+use POE qw(Component::Server::DNS);
 use strict;
 use warnings;
 use Data::Dumper;
@@ -17,20 +17,42 @@ sub spawn {
   print Data::Dumper->Dump([\%args],[qw($args)]);
 
   $self->{session_id} = POE::Session->create(
+	args => [ \%args,
+	],
 	object_states => [
-	  $self => [ qw(_start _stop complete step_a step_b step_c) ],
+	  $self => [ qw(_start _stop complete step_a step_b step_c query_handler resolver_response) ],
 	],
   );
   return $self;
 }
 
-# query request handler
-sub query {
-  # posts to each resolver object with a postback of resolver return state - fallthrough or hit - postback in ::Server session stores status in _HEAP per socket/port pair
-  # when hit, 
+# query request handler - this is hit by POE::Component::Client::DNS
+sub query_handler {
+  my ($self, $kernel, $heap, $session, $qname,$qclass,$qtype,$callback,$origin) = @_[OBJECT, KERNEL, HEAP, SESSION,ARG0..ARG4];
+  my ($rcode, @ans, @auth, @add);
+  printf STDERR "Server query_handler: QO: %s QC: %s QT: %s QN: %s\n",$origin,$qclass,$qtype,$qname;
+
+  foreach my $sink (@{$heap->{greysink}->{sinks}}) {
+    # XXX RGH FIXME post w/ postback to resolver_response
+    my $postback = $session->postback("resolver_response","dns_connection_socketpair_maybe");
+    $kernel->post($sink,"lookup",$postback,$qname,$qclass,$qtype);
+  }
+
+  if ($qtype eq "A") {
+    my ($ttl, $rdata) = (3600, "10.1.2.3");
+    push @ans, Net::DNS::RR->new("$qname $ttl $qclass $qtype $rdata");
+    $rcode = "NOERROR";
+  } else {
+    $rcode = "NXDOMAIN";
+  }
+
+  $callback->($rcode, \@ans, \@auth, \@add, { aa => 1 });
+  undef;
 }
 
 sub resolver_response {
+  my ($self, $kernel, $heap, $session, $passthru,$passback) = @_[OBJECT, KERNEL, HEAP, SESSION, ARG0, ARG1];
+  print Data::Dumper->Dump([$passthru,$passback],[qw($passthru $passback)]);
   # event listening for resolver postbacks - one event per resolver
   # stores status in _HEAP per socket/port pair and resolver friendly name
   # ARGS: resolver, hit?, record_ref
@@ -42,9 +64,25 @@ sub _stop {#{{{
 }#}}}
 
 sub _start {#{{{
-  my ($self, $kernel, $heap, $session) = @_[OBJECT, KERNEL, HEAP, SESSION];
+  my ($self, $kernel, $heap, $session, $args_ref) = @_[OBJECT, KERNEL, HEAP, SESSION, ARG0];
   print "Session ", $session->ID, " has started.\n";
   $kernel->alias_set("greysink");
+  print Data::Dumper->Dump([$args_ref],[qw($greysink_server_args)]);
+
+  # save off our "child" sink aliases
+  $heap->{greysink}->{sinks} = $args_ref->{sink_aliases};
+
+  # XXX make DNS server port configurable through ARGS
+  my $dns_server = POE::Component::Server::DNS->spawn( alias => 'dns_server',port=>5252 );
+
+  # set up DNS listener
+  $kernel->post( 'dns_server', 'add_handler', {
+      session => 'greysink',
+      event => 'query_handler',
+      label => 'ZA_WARUDO',
+      match => '.',
+  });
+
   my $postback = $session->postback("complete","dns_connection_socketpair_maybe");
   my ($qname,$qclass,$qtype) = qw(foobar.com IN A);
   $kernel->yield('step_a',$qname,$qclass,$qtype,$postback);
--- poe/lib/POE/Component/Greysink/Sink.pm
+++ poe/lib/POE/Component/Greysink/Sink.pm
@@ -2,6 +2,7 @@ package POE::Component::Greysink::Sink;
 use warnings;
 use strict;
 use POE;
+use Data::Dumper;
 
 sub spawn {
   my $class = shift;
@@ -10,9 +11,9 @@ sub spawn {
   print Data::Dumper->Dump([\%args],[qw($args)]);
 
   $self->{session_id} = POE::Session->create(
-        args => [ ],
+        args => [ $args{alias} ],
         object_states => [
-          $self => [ qw(_start _stop) ],
+          $self => [ qw(_start _stop lookup) ],
         ],
   );
   return $self;
@@ -20,7 +21,7 @@ sub spawn {
 
 sub _start {#{{{
   my ($self, $kernel, $heap, $session, $alias) = @_[OBJECT, KERNEL, HEAP, SESSION, ARG0];
-  print "Session ", $session->ID, " has started.\n";
+  print "Sink Session ($alias) ", $session->ID, " has started.\n";
   $kernel->alias_set($alias);
   my $postback = $session->postback("complete","dns_connection_socketpair_maybe");
   my ($qname,$qclass,$qtype) = qw(foobar.com IN A);
@@ -31,5 +32,11 @@ sub _stop {#{{{
   print "Session ", $_[SESSION]->ID, " has stopped.\n";
 }#}}}
 
-
+sub lookup {#{{{
+  my ($self, $kernel, $heap, $session, $postback,$qname,$qclass,$qtype) = @_[OBJECT, KERNEL, HEAP, SESSION, ARG0..ARG4];
+  my ($alias) = $kernel->alias_list($session);
+  print "Lookup of $qname in $alias\n";
+  print Data::Dumper->Dump([$postback,$qname,$qclass,$qtype],[qw($postback $qname $qclass $qtype)]);
+  
+}
 1;
